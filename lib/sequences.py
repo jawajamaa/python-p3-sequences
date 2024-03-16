ec0d07ac1638f03107f611b96de9e51b98ed3a7c
Timothy Ryon <tim@timryon.com>
2024-03-16 15:18:10 -0400

    finished lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -11,15 +11,21 @@ def print_fibonacci(length):
         fib_seq.append(1)
         print(fib_seq)
     else:
-        for idx in range(length):
-                breakpoint()
-                fib_seq.append(0)
-                fib_seq.append(1)
+        fib_seq.append(0)
+        fib_seq.append(1)
+        sum = fib_seq[-2] + fib_seq[-1]
+        fib_seq.append(sum)
+        if length == 3: 
+             print(fib_seq)
+             return
+        elif length > 3:
+            for idx in range(length-3):
+                #breakpoint()
                 sum = fib_seq[-2] + fib_seq[-1]
                 fib_seq.append(sum)
         print(fib_seq)
         
-
+print_fibonacci(1)
 # below in place of line 5?
         #idx = len(fib_seq)
         
